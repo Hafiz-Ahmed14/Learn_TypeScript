a19f553acb5712f400bba399f593124dbbbca2b4
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2026-07-13 10:52:37 +0600

    First Program of TS

---
+++ first_program.ts
@@ -0,0 +1,12 @@
+let myname = "Hafiz Ahmed";
+console.log(myname);
+
+let userName:string;
+userName = "Hafiz Ahmed";
+console.log(userName);
+
+function AddTwoNumber(num1:number, num2:number) {
+    return num1 + num2;
+}
+
+console.log(AddTwoNumber(10, 20));
