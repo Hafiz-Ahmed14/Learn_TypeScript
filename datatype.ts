3d1548c44bc39f51f63bc14bb9f32dda1cf0baf7
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2026-07-13 10:59:42 +0600

    add data type

---
+++ datatype.ts
@@ -0,0 +1,14 @@
+//Built in Data Type: 
+// Number, string, boolean, void, undefined, null
+
+let userId: number;
+let firstName: string;
+let lastName : string;
+let isActive : boolean;
+
+userId = 10;
+firstName = "hafiz";
+lastName = "ahmed"
+isActive = true;
+
+console.log(`Your Id: ${userId}, userName : ${firstName} ${lastName}, Your account is: ${isActive}`);
